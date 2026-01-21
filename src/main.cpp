17131c91a0faf0d71266160127214a2918d403cf
SillyGoose210 <koenmichaelson@gmail.com>
2026-01-20 18:44:36 -0700

    Pyramid working!

--- src/main.cpp
+++ src/main.cpp
@@ -15,3 +15,63 @@ If I was to type 3 it would show
 * * * *
 
 */
+
+#include <iostream>
+#include <string>
+
+int main() {
+
+    // Delared Variable
+    int pyramidHeight = 0;
+    bool keepBuilding = true;
+
+    while (keepBuilding) {
+
+        //Initial user input
+        std::cout << "Type in how tall you would like your pyramid!\n";
+        std::cin >> pyramidHeight;
+
+        if (pyramidHeight == -1) {
+            break;
+        }
+
+        // Main Iteration for constucting each row
+
+        for (int row = 1; row <= pyramidHeight; row++) {
+
+
+            // Calulate empty spaces
+            int emptySpaces = (pyramidHeight + 1) - row;
+
+            // Places each empty space required
+            for (int i = 1; i < emptySpaces; i++) {
+
+                std::cout << " ";
+            }
+
+            // Places the stars
+            for (int star = 0; star < row; star++) {
+
+                std::cout << " *";
+
+            }
+
+            //Starts new row
+            std::cout << " \n";
+
+        }
+
+        // Explain Exiting Loop
+        std::cout << "\n" << "Type in -1 to exit\n";
+
+
+
+
+    }
+
+
+
+   
+
+
+}
